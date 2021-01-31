8c6d07b66f1b10f92110ddbc289583e36b0d48d0
Ahmed <ahmed70000@gmail.com>
2021-01-31 05:46:58 +0200

    Added Flask + Constraints

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for, session 
+from flask_mysqldb import MySQL 
+import MySQLdb.cursors 
+import re 
+app = Flask(__name__) 
+  
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'BackEndTask'
+  
+  
+mysql = MySQL(app) 
+  
+  
+@app.route('/') 
+@app.route('/get', methods=['GET'])
+def get():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+    cid =request.args.get('id')
+    try:
+        string = int(cid)
+    except ValueError:
+        return "Enter a valid ID", 400
+    cursor.execute('SELECT * FROM customer where CID =%s',(cid))
+    account = cursor.fetchone()
+    if not account:
+        return "User not found", 400
+    
+    return(account)
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+    cid =request.args.get('id')
+    
+    try:
+        string = int(cid)
+    except ValueError:
+        return "Enter a valid ID", 400
+    cursor.execute('Select COUNT(*) from (SELECT * from customer WHERE CID=%s) AS T',(cid))
+    if (cursor.fetchone()['COUNT(*)']==0):
+        return "No corresponding Customer", 400
+
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM customer WHERE CID = %s" % (cid))
+    cur.connection.commit()    
+    return("account")
+
+@app.route('/add', methods=['POST'])
+def add():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+    name =request.args.get('name')
+    
+    if name.isalpha()==False:
+        return "Enter only letters", 400
+    cursor.execute('insert into customer (Name) values("%s")',(name))
+    
+    cursor.connection.commit()    
+    return("account")
--- task.sql
+++ task.sql
@@ -2,30 +2,30 @@ CREATE DATABASE BackEndTask;
 USE BackEndTask;
 
 CREATE TABLE Size (
-    SIZEID INT UNSIGNED  NOT NULL,
+    SIZEID INT UNSIGNED  NOT NULL auto_increment,
     VehicleSize varchar(255)  NOT NULL,
     PRIMARY KEY(SIZEID));
 CREATE TABLE Vehicle (
-    ID INT UNSIGNED  NOT NULL,
+    ID INT UNSIGNED  NOT NULL auto_increment,
     Size  INT UNSIGNED,
     Model varchar(255)  NOT NULL,
     PRIMARY KEY(ID),
-    foreign key(Size) references Size(SIZEID));
+    foreign key(Size) references Size(SIZEID) ON delete restrict ON update cascade);
 
 CREATE TABLE Customer (
-    CID INT UNSIGNED  NOT NULL,
+    CID INT UNSIGNED  NOT NULL auto_increment,
     Name varchar(255)  NOT NULL,
     PRIMARY KEY(CID));
 
 CREATE TABLE Booking (
-    BookingID int UNSIGNED NOT NULL,
+    BookingID int UNSIGNED NOT NULL auto_increment,
     CustomerID int UNSIGNED ,
     VehicleID int UNSIGNED ,
     Datebooked DATE NOT NULL,
     DateReturned DATE NOT NULL,
     PRIMARY KEY (BookingID),
-    FOREIGN KEY (CustomerID) REFERENCES Customer(CID),
-    FOREIGN KEY (VehicleID) REFERENCES Vehicle(ID)
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CID) ON DELETE CASCADE ON update cascade,
+    FOREIGN KEY (VehicleID) REFERENCES Vehicle(ID) ON DELETE CASCADE ON update cascade
 );
 
 
